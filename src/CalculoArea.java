754794b89de6234f2013e28ffafb61b6a99ba53f
juan jose <jquinteros2022@gmail.com>
2025-03-06 13:01:49 -0500

    se agrega ejercicio valores magicos

---
+++ src/CalculoArea.java
@@ -0,0 +1,14 @@
+public class CalculoArea {
+    
+        
+        public static final double PI = 3.14159;
+        public static final double RADIO = 5.0;
+        
+        public static void main(String[] args) {
+            double area = PI * RADIO * RADIO;
+            
+            System.out.println("El radio del círculo es: " + RADIO);
+            System.out.println("El área del círculo es: " + area);
+        }
+    }
+    
