2e3be4c9fc6d1044ca1800016589c18eef4651ee
juan jose <jquinteros2022@gmail.com>
2025-03-06 12:17:56 -0500

    se agrega ejercicio practico

---
+++ src/Practico.java
@@ -0,0 +1,18 @@
+public class Practico {
+    
+        public static void main(String[] args) {
+            
+            byte edad = 25; 
+            float altura = 1.75f; 
+            char inicialDelNombre = 'J'; 
+            boolean esMayorDeEdad = true; 
+            
+            // Imprimir valores
+            System.out.println("Edad: " + edad + " años");
+            System.out.println("Altura: " + altura + " metros");
+            System.out.println("Inicial del nombre: " + inicialDelNombre);
+            System.out.println("Es mayor de edad: " + esMayorDeEdad);
+        }
+    }
+    
+
