cad244e572c1d473a7c801f92624eff21f984404
juan jose <jquinteros2022@gmail.com>
2025-03-07 06:46:39 -0500

    ejercicio entrada y salida de datos

---
+++ src/LeerDecimal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeerDecimal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ingrese un numero decimal: ");
+        float numDecimal = scanner.nextFloat();
+
+        System.out.print("el numero decimal ingresado es: " + numDecimal);
+
+        scanner.close();
+    }
+
+}
