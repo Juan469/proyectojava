a6f17ffad6d592955951759baef8ce1f47633af3
juan jose <jquinteros2022@gmail.com>
2025-03-07 21:15:34 -0500

    Ejercicios practicos estructuras de control

---
+++ src/ejercicio2.java
@@ -0,0 +1,16 @@
+public class ejercicio2 {
+   
+            public static void main(String[] args) {
+                int numPares = 0;
+        
+                for (int i = 1; i <= 100; i++) {
+                    if (i % 2 == 0) {
+                        numPares++;
+                    }
+                }
+        
+                System.out.println(" los números pares entre 1 y 100 son: " + numPares);
+            }
+        }
+
+
